271e691e9d742707aae964d71548f43f11aad087
ThomasBrets <thomas15501331@gmail.com>
2023-10-30 13:05:09 -0300

    initial Header, add logo, img, font. colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+// Logo
+import logo from "../assets/img/logo.png";
+
+// Utils
+import { scrollToSection } from "../utils/ScrollToSection";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [header, setHeader] = useState(false);
 
-export default Header
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        header ? "bg-primary py-6 shadow-lg" : "bg-transparent py-8"
+      } fixed z-50 w-full transition-all duration-500`}
+    >
+      <div className="container mx-auto  gap-y-6 flex flex-col items-center lg:flex-row lg:justify-between lg:gap-y-0">
+        <a href="/">
+          <img src={logo} alt="logo" className="w-[140px] h-[140px] rounded-full p-2" />
+        </a>
+        <nav className={`${header ? "text-secondary" : "text-black"} font-primary flex gap-x-4 uppercase text-[18px] tracking-3px items-center lg:gap-x-8`}>
+          <div href="" onClick={() => scrollToSection('#home')} className="hover:text-secondary-hover transition cursor-pointer" >Home</div>
+          <div href="" onClick={() => scrollToSection('#history')} className="hover:text-secondary-hover transition cursor-pointer" >Nosotros</div>
+          <div href="" onClick={() => scrollToSection('#menu')} className="hover:text-secondary-hover transition cursor-pointer" >Menu</div>
+          <a href="/events" className="hover:text-secondary-hover transition">Eventos</a>
+          <div href="" onClick={() => scrollToSection('#contact')} className="hover:text-secondary-hover transition cursor-pointer">Reserva</div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary:'Caveat',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1140px',
+    },
+    extend: {
+      colors: {
+        primary: '#eb2023',
+        secondary: {
+          DEFAULT: '#00944a',
+          hover: '#1a9f5c',
+        },
+      },
+      backgroundImage: {
+        restaurant: "url('./assets/img/restaurant.png')",
+      },
+    },
   },
   plugins: [],
 }
