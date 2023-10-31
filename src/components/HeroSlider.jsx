0b067e502318e167cb6b432e4f7a981b4f2c71f0
ThomasBrets <thomas15501331@gmail.com>
2023-10-31 10:08:30 -0300

    Start HeroSlider

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import React from "react";
+
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+// import required modules
+import { EffectFade, Autoplay } from "swiper/modules";
+
+// Utils
+import { scrollToSection } from "../utils/ScrollToSection";
 
 const HeroSlider = () => {
   return (
